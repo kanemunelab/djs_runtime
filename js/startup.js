9c938db94b72fd82b9409e7838b182002b8a48c3
y_honda <ht13a084@oecu.jp>
2019-01-16 13:00:05 +0900

    opt=moveslowが指定された時にタートルをゆっくり動くタートルに変更するように変更。

--- js/startup.js
+++ js/startup.js
@@ -3,4 +3,13 @@ function isinclude(arr,name){var res=false;arr.forEach(function(e){if(e==name)re
 function startup(options){
   console.log(options);
   if(isinclude(options,"grid"))grid_show();
+
+	if(isinclude(options,"moveslow")){
+		root["_turtle"]=root["turtle"];
+		root["turtle"]=root["create"]();
+		root["turtle"]["create"]=dtlbind(root,function(){
+			return root["_turtle"]["create"]()["moveSlow"]();
+		});
+		root["addAlias"]("turtle","Turtle","タートル","たーとる");
+	}
 }
